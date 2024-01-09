eaab5e81a6a6c3180ddf837241c7de46209b670d
rgarro <rgarro@gmail.com>
2024-01-09 07:21:19 -0600

    original line endings in your w..

--- www/js/ridesapp.js
+++ www/js/ridesapp.js
@@ -27,13 +27,21 @@
 class ridesApp {
 
      labelsUrl = "labelsUrls";
+     testUrl = "https://helloworld-bca576to3a-uc.a.run.app/"
 
     constructor(){
 console.log("android compilara ...");
     }
 
+    runTest(){
+        $.get(this.testUrl, function(data){
+            $(".result").html(data);
+            alert("Load was performed. "+ data);
+          });
+    }
+
     getLabels(){
-        console.log("label: "+this.labelsUrl);
+        console.log("label: "+this.labelsUrl  );
     }
 
     setLabel(id){
