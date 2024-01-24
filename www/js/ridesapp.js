6ae493cb2d3ee6ae0b3d804084b32991e76ce1fc
rgarro <rgarro@gmail.com>
2024-01-24 09:28:57 -0600

    Je bois le cafe noir , monsieur Android manges du pain avec le fromage  ...

--- www/js/ridesapp.js
+++ www/js/ridesapp.js
@@ -16,6 +16,8 @@
  * A football player and his friends travel to the planet Mongo and 
  * find themselves fighting the tyranny of Ming the Merciless to save Earth.
  * mongodb vs firebase
+ * Le Cafe Noir Android Company 
+ * THIS IS THE PROOF OF CONCEPT OF A PYMES PROPOSAL.  
  * 
  * @author Rolando<rgarro@gmail.com>
  */
@@ -24,14 +26,32 @@ class ridesApp {
      labelsUrl = "labelsUrls";
      testUrl = "https://helloworld-bca576to3a-uc.a.run.app/";
      atlasUrl = "https://us-east-1.aws.data.mongodb-api.com/app/data-uizjf/endpoint/data/v1/";
+     atlasAuthUrl = "https://realm.mongodb.com/api/client/v2.0/app/data-uizjf/auth/providers/local-userpass/login";
      atlasUser = "rolandogarro4@gmail.com";
      atlasPassword = "123queso";
      collection = "Rides";
      database = "ridesappdb";
      dataSource = "AtlasCluster";
+     ACCESS_TOKEN = "";
+     is_authenticated = false; 
 
     constructor(){
 //console.log("android compilara ...");
+    }
+
+    authenticate(){
+        $.ajax({
+            url: this.atlasAuthUrl,
+            type: 'post',
+            data: {
+                username: this.atlasUser,
+                password: this.atlasPassword
+            },
+            dataType: 'json',
+            success: (function (data) {
+                console.log(data);//Je bois le cafe noir , monsieur Android mange du pain avec le fromage  ...
+            }).bind(this)
+        });
     }
 
     runTest(){
@@ -42,7 +62,22 @@ class ridesApp {
     }
 
     getRides(){
-        url = this.atlasUrl + "action/findAll";
+        find_url = this.atlasUrl + "action/findAll";
+        $.ajax({
+            url: find_url,
+            type: 'post',
+            data: {
+                access_token: 'XXXXXXXXXXXXXXXXXXX'
+            },
+            headers: {
+                "Access-Control-Request-Headers": '*',
+                "Authorization": 'Bearer ' + this.ACCESS_TOKEN
+            },
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+            }
+        });
 
     }
 
