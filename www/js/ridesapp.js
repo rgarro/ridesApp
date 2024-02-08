61ab7872eabc89722a9a5c66c9c6281289d27565
rgarro <rgarro@gmail.com>
2024-02-08 16:55:07 -0600

    je mange le poulet rouge..

--- www/js/ridesapp.js
+++ www/js/ridesapp.js
@@ -27,8 +27,6 @@ class ridesApp {
 
      labelsUrl = "labelsUrls";
      testUrl = "https://helloworld-bca576to3a-uc.a.run.app/";
-     //https://us-east-1.aws.data.mongodb-api.com/app/data-uizjf/endpoint/data/v1
-     //https://us-east-1.aws.data.mongodb-api.com/app/data-uizjf/endpoint/data/v1
      atlasUrl = "https://us-east-1.aws.data.mongodb-api.com/app/data-uizjf/endpoint/data/v1/";
      atlasAuthUrl = "https://realm.mongodb.com/api/client/v2.0/app/data-uizjf/auth/providers/local-userpass/login";
      atlasUser = "rolandogarro4@gmail.com";
@@ -45,30 +43,33 @@ class ridesApp {
      rides_template;
      privateKey = "2e6e32a6-e65d-4a3a-a25e-74f6d5dfd851";
      publicKey = "gydalonr";
-    mayapikey = "j78V0iGxREyDTzbV4BLid2IdbC8RGidekEYviWAmYeOTFjtwxbmg1Yfo65wsgrTC";
-    REALM_PID = "data-uizjf";//"65aea7dcf39cfd0112e90472";
-    REALM_APP;//ridesappdb-ffygd
-    mongodb;
-    user;
+     mayapikey = "j78V0iGxREyDTzbV4BLid2IdbC8RGidekEYviWAmYeOTFjtwxbmg1Yfo65wsgrTC";
+     REALM_PID = "data-uizjf";//"65aea7dcf39cfd0112e90472";
+     REALM_APP;//ridesappdb-ffygd
+     mongodb;
+     user;
 
 
     constructor(){
-//console.log("el android compilara ...");
+        //console.log("el android compilara ...");//que ladre el DES(madre)
         this.REALM_APP = new Realm.App({ id: this.REALM_PID });
-        console.log("realm:"+this.REALM_APP);
+        //console.log("realm:"+this.REALM_APP);
+        this.compileTemplates();
+    }
+
+    compileTemplates(){
+        var source = document.getElementById("rides-template").innerHTML;
+        this.rides_template = Handlebars.compile(source);
     }
 
     async myRealmAuthenticate(){
-        console.log("start realm auth:");
         //const credentials = Realm.Credentials.emailPassword(this.atlasUser,this.atlasPassword);
         // Create an API Key credential
         const credentials = Realm.Credentials.apiKey(this.mayapikey);
         // Authenticate the user
         this.user = await this.REALM_APP.logIn(credentials);
         // App.currentUser updates to match the logged in user
-        console.log(this.user);
         this.mongodb = this.user.mongoClient(this.dataSource);
-        console.log(this.mongodb);
         this.is_authenticated = true;
     }
 
@@ -106,14 +107,16 @@ class ridesApp {
     }
 
     async getRides(){
-        console.log("Je mange le poulet rouge ..");
+        //console.log("Je mange le poulet rouge ..");
         const ridesCollection = this.mongodb.db(this.database).collection("Rides");
-        const ridesData = await ridesCollection.find({is_active : true});
-console.log(ridesData);
-        //var source = document.getElementById("rides-template").innerHTML;
-        //this.rides_template = Handlebars.compile(source);
-        //var html = this.rides_template(context);
-        //$(".rides-list-cont").html(data);
+        var ridesData = await ridesCollection.find({is_active : true});
+        for (var i = 0; i < ridesData.length; i++) { 
+            const date = new Date(ridesData[i].ridedate * 1000);
+            ridesData[i].fixedDate = date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate()+ " " +date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+          }
+ console.log(ridesData);         
+        var html = this.rides_template({rides:ridesData});        
+        $(".rides-list-cont").html(html);
     }
 
     getLabels(){
