1494ed7b887158d8e8c4e5cbc8953c596d10e8bb
rgarro <rgarro@gmail.com>
2024-02-13 12:53:58 -0600

    quincho es playo IV...

--- www/js/Ticker.js
+++ www/js/Ticker.js
@@ -1,7 +1,15 @@
 /**
  *   En tibas no regalan casas ni prestan cocheras a merodeadores chilenos.. 
  * Un programa de quince aserciones gritando quincho es playo en la catedral metropolitana ..
- *
+ * Yo llevaba una piedra de lugares en los que quincho marco en lista a cacheteados en capilla
+ * el padre aceptaba retos, yo reto que yo termino esto y quincho muere antes de los 56 años.. ja ja ja
+ * La asercion es que lanuza es gay y se considera culpable de haber intentado vender a quincho antes
+ *  yo vendi matar a quincho y ver la cara de recuperados a varios heredianos, a musulmanes ticos del mercado borbon.
+ * Vamos a poner una piedra de la Meca donde lo bautizaron y provocarle la muerte de no poder ir a heredia en la proxima vida
+ * por que el padre no puede ir a jerusalem y perdonarlo de vivir mas que el.
+ * muchos de la generacion de quincho son gays , asesinos y ladrones algunos van a recuperar la salud el dia de la muerte de quincho.
+ * quincho soplaba flauta en la parroquia del barrio y luego era un cochino testigo de jeova
+ * La ONG en la carcel de menores de San Isidro de Heredia paga ...
  * 
  * @author Rolando<rgarro@gmail.com>
  */
