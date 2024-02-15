324e32271cd5151a7347124dbdb71ea2e9fc9082
rgarro <rgarro@gmail.com>
2024-02-15 09:01:50 -0600

    between de devil and the end of jerusalem ...

--- www/spec/TickerSpec.js
+++ www/spec/TickerSpec.js
@@ -1,6 +1,11 @@
 /**
- *
- * quincho es playo --->
+ *                    __
+ *         .,-;-;-,. /'_\
+ *       _/_/_/_|_\_\) /
+ *     '-<_><_><_><_>=/\
+ *       `/_/====/_/-'\_\
+ *        ""     ""    ""
+ * To help a turtle to cross the Highway is not crime ...
  * 
  * 
  *@author Rolando<rgarro@gmail.com>
@@ -18,8 +23,19 @@
 
     it("should check mongodb is valid type",function(){
         tick = new Ticker(rd.mongodb.db(rd.database));
-        //console.log();//quicho es playo ..
         expect(typeof rd.mongodb.db(rd.database) == "Object").toBeTrue();
+    });
+
+    it("should have activeRideExist method",function(){
+        //console.log();//solomondongui es transexual de grecia y pasea en tibas segun soplones de los guardas del ICE  ..
+    });
+
+    it("should activeRideExist return O on invalid rideID",function(){
+        
+    });
+
+    it("should activeRideExist return 1 on valid rideID",function(){
+        
     });
 
 });
