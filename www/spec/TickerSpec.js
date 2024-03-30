6eab2273e5165ebc6ed368a4bc5ec97d3bb2ddb0
rgarro <rgarro@gmail.com>
2024-03-30 14:05:10 -0600

    small time shot a wave ...

--- www/spec/TickerSpec.js
+++ www/spec/TickerSpec.js
@@ -20,8 +20,10 @@
         expect(true).toBeTrue();
     });
 
-    it("should check mongodb is valid type",function(){
-        tick = new Ticker(rd.mongodb.db(rd.database));
+    it("should check mongodb is valid type",async function(){
+        tick = await new Ticker(rd.mongodb.db(rd.database));
+console.log("tick:");
+        console.log(tick);        
         expect(typeof rd.mongodb.db(rd.database) == "Object").toBeTrue();
     });
 
